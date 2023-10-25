caef1451d2a6c3cac44f766bb3cdcb0b641c0fbf
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-25 12:20:30 -0300

    feat(IQNet-App/client/src/App):added axios default URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 import Home from "./views/home/Home.jsx";
 import Administrator from "./views/administrator/Administrator.jsx";
 import "./App.module.scss";
+
+axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
   return (
