cc1786561d0da0af2320589368b7fa86190b235c
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-25 09:56:16 -0300

    feat(IQNet-App/server):added server configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,38 @@
-const express = require("express");
-const countriesRouter = require("./routes/countryRouter");
-const activitiesRouter = require("./routes/activityRouter");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./routes/index.js');
+const mercadopago = require("mercadopago");
+
+require('./db.js');
 
 const server = express();
 
-server.use(morgan("dev"));
-server.use(express.json());
-server.use(cors({
-  origin: [
-    'https://pi-countries-api-flax.vercel.app',
-    'https://proyectofinal-henry-production.up.railway.app',
-    'http://localhost:5173'
-  ]
-}));
+server.name = 'API';
 
-server.use('/countries', countriesRouter);
-server.use('/activities', activitiesRouter);
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+server.use(cors());
+
+server.use('/', router);
+
+server.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
