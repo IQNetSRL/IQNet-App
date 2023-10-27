4f991e29dffe849643175e1b30a24f1d7d3ec54b
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-27 11:38:07 -0300

    feat(IQNet-App/client/src/routes):added Accounts routes

--- server/src/routes/accountsRoute.js
+++ server/src/routes/accountsRoute.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const postAccount = require("../handlers/Accounts/postAccount.js");
+const getAccounts = require("../handlers/Accounts/getAccounts.js");
 
 const accountsRouter = Router();
 
 accountsRouter.post("/", postAccount);
+
+accountsRouter.get("/", getAccounts);
 
 module.exports = accountsRouter;
