6da7bc311177d6a7ae9e25f12595241b8195a4d5
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-27 11:18:31 -0300

    feat(IQNet-App/client/src/components):added LogoutButton component

---
+++ server/src/models/Accounts.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Accounts",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      level: {
+        type: DataTypes.ENUM(
+          "admin",
+          "service",
+        ),
+        allowNull: false,
+      },
+    },
+    { timestamps: true }
+  );
+};
