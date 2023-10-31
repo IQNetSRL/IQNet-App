fc7735020e46364db7382b0f26ed8079df39e6fa
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-31 11:48:02 -0300

    feat(IQNet-App/server/src/routes):added put information routes

--- server/src/routes/informationRoute.js
+++ server/src/routes/informationRoute.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
 const postInformation = require("../handlers/Information/postInformation.js");
 const getInformation = require("../handlers/Information/getInformation.js");
+const putInformation = require("../handlers/Information/putInformation.js");
 
 const informationRouter = Router();
 
 informationRouter.get("/", getInformation);
 
 informationRouter.post("/", postInformation);
+
+informationRouter.put("/:id", putInformation);
 
 module.exports = informationRouter;
