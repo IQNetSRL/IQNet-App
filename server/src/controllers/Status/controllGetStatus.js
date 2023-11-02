5e2f1d9e40ae704d14bd5c5537b4d761aee9ca5c
Franco León Cáceres <francocaceres008@gmail.com>
2023-11-02 11:36:58 -0300

    feat(IQNet-App/server/src/controllers):modified Status GET controller to get by id too

--- server/src/controllers/Status/controllGetStatus.js
+++ server/src/controllers/Status/controllGetStatus.js
@@ -1,9 +1,24 @@
 const { Status } = require("../../db.js");
 
 const controllGetStatus = async (req) => {
-  const allStatus = await Status.findAll();
+  const { statusId } = req.query;
 
-  return allStatus;
+  const where = {};
+
+  if (!statusId) {
+    const status = await Status.findAll();
+    return status;
+  }
+
+  if (statusId) {
+    where.id = statusId;
+  }
+
+  const status = await Status.findAll({
+    where,
+  });
+
+  return status;
 };
 
 module.exports = controllGetStatus;
