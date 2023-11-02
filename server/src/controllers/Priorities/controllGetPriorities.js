b3ed7001b315e4ceb96038006c22b85ffd292678
Franco León Cáceres <francocaceres008@gmail.com>
2023-11-02 11:34:22 -0300

    feat(IQNet-App/server/src/controllers):modified Priorities GET controller to get by id too

--- server/src/controllers/Priorities/controllGetPriorities.js
+++ server/src/controllers/Priorities/controllGetPriorities.js
@@ -1,9 +1,24 @@
 const { Priorities } = require("../../db.js");
 
 const controllGetPriorities = async (req) => {
-  const allPriorities = await Priorities.findAll();
+  const { prioritiesId } = req.query;
 
-  return allPriorities;
+  const where = {};
+
+  if (!prioritiesId) {
+    const priorities = await Priorities.findAll();
+    return priorities;
+  }
+
+  if (prioritiesId) {
+    where.id = prioritiesId;
+  }
+
+  const priorities = await Priorities.findAll({
+    where,
+  });
+
+  return priorities;
 };
 
 module.exports = controllGetPriorities;
