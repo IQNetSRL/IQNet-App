4242ac556fec63c294801fb7b342ec35def15391
Franco León Cáceres <francocaceres008@gmail.com>
2023-11-06 12:46:05 -0300

    feat(IQNet-App/server/src/controllers):fixed Status GET controller

--- server/src/controllers/Status/controllGetStatus.js
+++ server/src/controllers/Status/controllGetStatus.js
@@ -1,24 +1,30 @@
 const { Status } = require("../../db.js");
 
 const controllGetStatus = async (req) => {
-  const { statusId } = req.query;
+  if (req) {
+    const { statusId } = req.query;
 
-  const where = {};
+    const where = {};
 
-  if (!statusId) {
-    const status = await Status.findAll();
+    if (!statusId) {
+      const status = await Status.findAll();
+      return status;
+    }
+
+    if (statusId) {
+      where.id = statusId;
+    }
+
+    const status = await Status.findAll({
+      where,
+    });
+
     return status;
   }
 
-  if (statusId) {
-    where.id = statusId;
-  }
+  const allStatus = await Status.findAll();
 
-  const status = await Status.findAll({
-    where,
-  });
-
-  return status;
+  return allStatus;
 };
 
 module.exports = controllGetStatus;
