7c4f33ad68e5d54c3f37dd967855f193168e5aad
Franco León Cáceres <francocaceres008@gmail.com>
2023-11-10 16:59:35 -0300

    feat(IQNet-App/client/src/components):added Ticket component

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,80 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteTicket } from "../../redux/actions.js";
+import Ticket from "../../components/ticket/Ticket.jsx";
 import styles from "./Home.module.scss";
 
 const Home = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const allTickets = useSelector((state) => state.someReducer.allTickets);
-  const allAreas = useSelector((state) => state.someReducer.allAreas);
-  const allCategories = useSelector((state) => state.someReducer.allCategories);
-  const allStatus = useSelector((state) => state.someReducer.allStatus);
-  const allPriorities = useSelector((state) => state.someReducer.allPriorities);
 
   const handleNavigate = () => {
     navigate("/admin");
-  };
-
-  const getAreaNameById = (id, allAreas) => {
-    const area = allAreas.find((area) => area.id === id);
-    return area ? area.name : "";
-  };
-
-  const getCategoryNameById = (id, allCategories) => {
-    const category = allCategories.find((category) => category.id === id);
-    return category ? category.name : "";
-  };
-
-  const getStatusNameById = (id, allStatus) => {
-    const status = allStatus.find((status) => status.id === id);
-    return status ? status.name : "";
-  };
-
-  const getPriorityNameById = (id, allPriorities) => {
-    const priority = allPriorities.find((priority) => priority.id === id);
-    return priority ? priority.name : "";
-  };
-
-  const handleDeleteTicket = (id) => {
-    dispatch(deleteTicket(id));
   };
 
   return (
     <section className={styles.sectionHome}>
       <h1>Home</h1>
       <button onClick={handleNavigate}>ir a administrador</button>
-      <div className={styles.ticketList}>
-        <h2>Lista de Tickets</h2>
-        <ul>
-          {allTickets.map((ticket) => (
-            <li key={ticket.id}>
-              <p>Área: {getAreaNameById(ticket.AreaId, allAreas)}</p>
-              <p>
-                Categoría:{" "}
-                {getCategoryNameById(ticket.CategoryId, allCategories)}
-              </p>
-              <p>Estado: {getStatusNameById(ticket.StatusId, allStatus)}</p>
-              <p>
-                Prioridad:{" "}
-                {getPriorityNameById(ticket.PriorityId, allPriorities)}
-              </p>
-              <p>Creador: {ticket.username}</p>
-              <p>Encargado: {ticket.responsable}</p>
-              <p>Cliente: {ticket.client}</p>
-              <p>Direccion: {ticket.address}</p>
-              <p>Descripcion: {ticket.text}</p>
-              <p>Comentarios: {ticket.comment}</p>
-              <button onClick={() => handleDeleteTicket(ticket.id)}>
-                Eliminar Ticket
-              </button>
-              <br />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h2>Lista de Tickets</h2>
+      <Ticket />
     </section>
   );
 };
