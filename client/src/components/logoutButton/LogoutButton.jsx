e7cef12a32fe228b0004b3b7b0d61b2dfef5b453
Franco León Cáceres <francocaceres008@gmail.com>
2023-12-01 00:12:48 -0300

    feat(IQNet-App/client/src/components/LogOutButton):added styles

--- client/src/components/logoutButton/LogoutButton.jsx
+++ client/src/components/logoutButton/LogoutButton.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { TbLogout2 } from "react-icons/tb";
 import styles from "./LogoutButton.module.scss";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button className={styles.logoutButton} onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button
+      className={styles.logoutButton}
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+    >
+      <span>
+        <TbLogout2 />
+      </span>
       Cerrar sesion
     </button>
   );
