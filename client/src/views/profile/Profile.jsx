8d31f009280e4c7734096fa30e591fb71a4f3830
Franco León Cáceres <francocaceres008@gmail.com>
2023-12-04 16:55:08 -0300

    feat(IQNet-App/client/src/views/Profile):added styles

--- client/src/views/profile/Profile.jsx
+++ client/src/views/profile/Profile.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { IoTicket } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
 import PropTypes from "prop-types";
 import Ticket from "../../components/ticket/Ticket";
 import styles from "./Profile.module.scss";
@@ -16,28 +18,34 @@ const Profile = (props) => {
     return <div>Cargando ...</div>;
   }
 
-  const handleNavigate = () => {
-    window.history.back();
-  };
-
   return (
     <section className={styles.sectionProfile}>
-      <h1>Perfil</h1>
-      <div>
+      <h1>
+        <span>
+          <FaUser />
+        </span>
+        Perfil
+      </h1>
+      <div className={styles.infoSection}>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <h2>
-          Rol:{" "}
-          {props.rol === "support"
-            ? "servicio tecnico"
-            : props.rol === "admin"
-            ? "administrador"
-            : props.rol === "sales" && "ventas"}
-        </h2>
+        <div className={styles.nameSection}>
+          <h2>{user.name}</h2>
+          <h3>
+            {props.rol === "support"
+              ? "servicio tecnico"
+              : props.rol === "admin"
+              ? "administrador"
+              : props.rol === "sales" && "ventas"}
+          </h3>
+          <h5>{user.email}</h5>
+        </div>
       </div>
-      <button onClick={handleNavigate}>volver</button>
-      <h2>Lista de Tickets</h2>
+      <div className={styles.titleContainer}>
+        <h2 className={styles.title}>Tu lista de Tickets</h2>
+        <span className={styles.ticketIcon}>
+          <IoTicket />
+        </span>
+      </div>
       <Ticket />
     </section>
   );
