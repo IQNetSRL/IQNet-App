328183527b25dcd34ac0b65aebba9326932ea0e2
Franco León Cáceres <francocaceres008@gmail.com>
2023-12-06 20:33:27 -0300

    feat(IQNet-App/client/src/views/FiltersPage):added some filters

--- client/src/views/filtersPage/FiltersPage.jsx
+++ client/src/views/filtersPage/FiltersPage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./FiltersPage.module.scss";
 
@@ -9,6 +9,26 @@ const FiltersPage = () => {
   const allCategories = useSelector((state) => state.someReducer.allCategories);
   const allStatus = useSelector((state) => state.someReducer.allStatus);
   const allPriorities = useSelector((state) => state.someReducer.allPriorities);
+  const [areas, setAreas] = useState(0);
+
+  const getValueNameById = (id, state) => {
+    const value = state.find((priority) => priority.id === id);
+    return value ? value.name : "";
+  };
+
+  function contarRepeticionesPorNombre(arrayDeObjetos) {
+    const contadorNombres = {};
+
+    arrayDeObjetos.forEach((objeto) => {
+      const nombre = objeto.responsable;
+      contadorNombres[nombre] = (contadorNombres[nombre] || 0) + 1;
+    });
+
+    return contadorNombres;
+  }
+
+  const responsables = contarRepeticionesPorNombre(allTickets);
+  console.log(responsables);
 
   return (
     <section className={styles.sectionLoby}>
@@ -16,6 +36,42 @@ const FiltersPage = () => {
         <h1>Todos los tickets</h1>
         <span>{allTickets.length}</span>
       </div>
+      <div>
+        <h1>Tickets abiertos</h1>
+        <span>
+          {
+            allTickets
+              .map((ticket) => getValueNameById(ticket.StatusId, allStatus))
+              .filter((e) => e === "abierto").length
+          }
+        </span>
+      </div>
+      <div>
+        <h1>Tickets cerrados</h1>
+        <span>
+          {
+            allTickets
+              .map((ticket) => getValueNameById(ticket.StatusId, allStatus))
+              .filter((e) => e === "cerrado").length
+          }
+        </span>
+      </div>
+      <div>
+        <h1>Tickets pendientes</h1>
+        <span>
+          {
+            allTickets
+              .map((ticket) => getValueNameById(ticket.StatusId, allStatus))
+              .filter((e) => e === "pendiente").length
+          }
+        </span>
+      </div>
+      {Object.entries(responsables).map(([propiedad, valor]) => (
+        <div key={propiedad}>
+          <h1>Tickets asignados a {propiedad}</h1>
+          <span>{valor}</span>
+        </div>
+      ))}
     </section>
   );
 };
