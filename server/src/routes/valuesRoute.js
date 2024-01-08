4333119270a40cd43883a62c2079ede66759ed98
Franco León Cáceres <francocaceres008@gmail.com>
2024-01-08 12:32:43 -0300

    feat(IQNet-App/server/src/):added areas, categories, status and priorities routes

--- server/src/routes/valuesRoute.js
+++ server/src/routes/valuesRoute.js
@@ -2,15 +2,19 @@ const { Router } = require("express");
 const deleteArea = require("../handlers/Areas/deleteArea.js");
 const getAreas = require("../handlers/Areas/getAreas.js");
 const postArea = require("../handlers/Areas/postArea.js");
+const putArea = require("../handlers/Areas/putArea.js");
 const deleteCategory = require("../handlers/Categories/deleteCategory.js");
 const getCategories = require("../handlers/Categories/getCategories.js");
 const postCategory = require("../handlers/Categories/postCategory.js");
+const putCategory = require("../handlers/Categories/putCategory.js");
 const deleteStatus = require("../handlers/Status/deleteStatus.js");
 const getStatus = require("../handlers/Status/getStatus.js");
 const postStatus = require("../handlers/Status/postStatus.js");
+const putStatus = require("../handlers/Status/putStatus.js");
 const deletePriority = require("../handlers/Priorities/deletePriority.js");
 const getPriorities = require("../handlers/Priorities/getPriorities.js");
 const postPriority = require("../handlers/Priorities/postPriority.js");
+const putPriority = require("../handlers/Priorities/putPriority.js");
 
 const valuesRouter = Router();
 
@@ -32,10 +36,18 @@ valuesRouter.get("/priorities", getPriorities);
 
 valuesRouter.delete("/areas/:id", deleteArea);
 
+valuesRouter.put("/areas/:id", putArea);
+
 valuesRouter.delete("/categories/:id", deleteCategory);
+
+valuesRouter.put("/categories/:id", putCategory);
 
 valuesRouter.delete("/status/:id", deleteStatus);
 
+valuesRouter.put("/status/:id", putStatus);
+
 valuesRouter.delete("/priorities/:id", deletePriority);
+
+valuesRouter.put("/priorities/:id", putPriority);
 
 module.exports = valuesRouter;
